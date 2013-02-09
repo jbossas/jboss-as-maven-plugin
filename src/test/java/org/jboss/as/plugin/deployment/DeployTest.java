f692252fb2414eb207863b3bc33eaa8d5dbd4392
Heinz Wilming <heinz.wilming@akquinet.de>
2013-02-09 16:59:00 +0100

    arquillina deploy test

---
+++ src/test/java/org/jboss/as/plugin/deployment/DeployTest.java
@@ -0,0 +1,18 @@
+package org.jboss.as.plugin.deployment;
+
+import org.jboss.as.plugin.AbstractItTestCase;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+
+public class DeployTest extends AbstractItTestCase {
+    
+    
+    @Test
+    public void testDeployWarArchive(){
+        
+        
+        
+    }
+
+}
